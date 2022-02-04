4b8751a7f49e82c670b9e96239ea0593748a69b7
moises56 <moiavil56@gmail.com>
2022-02-04 00:02:50 -0600

    web3+angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { StartComponent } from './components/start/start.component';
+import { TransferComponent } from './components/transfer/transfer.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/start', pathMatch: 'full' },
+  { path: 'start', component: StartComponent },
+  { path: 'transfer', component: TransferComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
